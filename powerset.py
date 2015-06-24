882376d90c6e960d5ab830f1b997cca185e109a7
chrisnatali <chris.natali@gmail.com>
2015-06-24 16:01:42 -0400

    renamed to be consistent with set theory lingo

--- powerset.py
+++ powerset.py
@@ -1,17 +1,17 @@
 #!/bin/python
 
-def all_subsets_rec(left, so_far, accum):
+def powerset_rec(left, so_far, accum):
     if len(left) == 0:
         accum.append(so_far)
     else:
         # find subsets that include first item of left
-        all_subsets_rec(left[1:], [left[0]] + so_far, accum)
+        powerset_rec(left[1:], [left[0]] + so_far, accum)
         # find subsets that do NOT include first item of left
-        all_subsets_rec(left[1:], so_far, accum)
+        powerset_rec(left[1:], so_far, accum)
 
 
-def all_subsets(items):
+def powerset(items):
     accum = []
-    all_subsets_rec(items, [], accum)
+    powerset_rec(items, [], accum)
     return accum
 
